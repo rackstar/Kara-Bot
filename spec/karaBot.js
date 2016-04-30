b4a17db14f21f66cc339d8aa6558d1acc6dfb381
Aaron Hildebrand <aaron.hildebrand@gmail.com>
2016-04-30 14:47:46 -0700

    (test) Add tests for current easter eggs

--- spec/karaBot.js
+++ spec/karaBot.js
@@ -90,6 +90,58 @@ describe('Responds to strings it knows', function () {
         }, 750);
       });
   });
+
+  it('replies to "life the universe and everything" with "42"', function (done) {
+    var text;
+    setTimeout(function () {
+      expect(text).to.be.a('string');
+      expect(text).to.equal('42');
+      done();
+    }, 1500);
+    chai.request('https://slack.com/api/chat.postMessage')
+      .post('?token=' + process.env.me + '&channel=' + process.env.botDirect
+        + '&text=life, the universe and everything&as_user=true&pretty=1')
+      .end(function (err, res) {
+        setTimeout(function () {
+          chai.request('https://slack.com/api/im.history')
+            .get('?token=' + process.env.me + '&channel=' + process.env.botDirect
+              + '&count=5&pretty=1')
+            .end(function (err, res) {
+              if (err) {
+                console.log('err ', err);
+              } else {
+                text = res.body.messages[0].text;
+              }
+            });
+        }, 750);
+      });
+  });
+
+  it('replies to "master code" with THE code', function (done) {
+    var text;
+    setTimeout(function () {
+      expect(text).to.be.a('string');
+      expect(text).to.equal('↑ ↑ ↓ ↓ ← → ← → Ⓑ Ⓐ START');
+      done();
+    }, 1500);
+    chai.request('https://slack.com/api/chat.postMessage')
+      .post('?token=' + process.env.me + '&channel=' + process.env.botDirect
+        + '&text=master code&as_user=true&pretty=1')
+      .end(function (err, res) {
+        setTimeout(function () {
+          chai.request('https://slack.com/api/im.history')
+            .get('?token=' + process.env.me + '&channel=' + process.env.botDirect
+              + '&count=5&pretty=1')
+            .end(function (err, res) {
+              if (err) {
+                console.log('err ', err);
+              } else {
+                text = res.body.messages[0].text;
+              }
+            });
+        }, 750);
+      });
+  });
 });
 
 describe('Does not respond to unrecognized strings', function () {
