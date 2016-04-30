c52c006bef2a9a5240c291a82499aa97a31ef078
Aaron Hildebrand <aaron.hildebrand@gmail.com>
2016-04-30 14:45:37 -0700

    (test) Add test for name storage

--- spec/karaBot.js
+++ spec/karaBot.js
@@ -38,6 +38,32 @@ describe('Responds to strings it knows', function () {
         }, 750);
       });
   });
+
+  it('can store name', function (done) {
+    var text;
+    setTimeout(function () {
+      expect(text).to.be.a('string');
+      expect(text).to.equal('Got it. I will call you ' + testName + ' from now on.');
+      done();
+    }, 1500);
+    chai.request('https://slack.com/api/chat.postMessage')
+      .post('?token=' + process.env.me + '&channel=' + process.env.botDirect
+        + '&text=my name is ' + testName + '&as_user=true&pretty=1')
+      .end(function (err, res) {
+        setTimeout(function () {
+          chai.request('https://slack.com/api/im.history')
+            .get('?token=' + process.env.me + '&channel=' + process.env.botDirect
+              + '&count=5&pretty=1')
+            .end(function (err, res) {
+              if (err) {
+                console.log('err ', err);
+              } else {
+                text = res.body.messages[0].text;
+              }
+            });
+        }, 750);
+      });
+  });
 });
 
 describe('Does not respond to unrecognized strings', function () {
