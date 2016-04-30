be321a226452ac95328c19744341f29a4423721c
Aaron Hildebrand <aaron.hildebrand@gmail.com>
2016-04-30 14:44:43 -0700

    (test) Add test for unrecognized entry

--- spec/karaBot.js
+++ spec/karaBot.js
@@ -39,3 +39,31 @@ describe('Responds to strings it knows', function () {
       });
   });
 });
+
+describe('Does not respond to unrecognized strings', function () {
+  it('does not reply to "jello"', function (done) {
+    var text;
+    setTimeout(function () {
+      expect(text).to.be.a('string');
+      expect(text).to.equal('jello');
+      done();
+    }, 1500);
+    chai.request('https://slack.com/api/chat.postMessage')
+      .post('?token=' + process.env.me + '&channel=' + process.env.botDirect
+        + '&text=jello&as_user=true&pretty=1')
+      .end(function (err, res) {
+        setTimeout(function () {
+          chai.request('https://slack.com/api/im.history')
+            .get('?token=' + process.env.me + '&channel=' + process.env.botDirect
+              + '&count=5&pretty=1')
+            .end(function (err, res) {
+              if (err) {
+                console.log('err ', err);
+              } else {
+                text = res.body.messages[0].text;
+              }
+            });
+        }, 750);
+      });
+  });
+});
