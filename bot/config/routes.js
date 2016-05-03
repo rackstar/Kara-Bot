a630fd86aa1b1fedf09a428d80e0d366bc2dde01
Rocky Murdoch <rockysta009@gmail.com>
2016-05-03 10:28:31 -0700

    (feat) Add github webhook route and error functions

--- bot/config/routes.js
+++ bot/config/routes.js
@@ -1,10 +1,24 @@
 var kara = require('../karabot');
+var github = require('../github/github');
+
+function errorLogger(error, req, res, next) {
+  // log the error then send it to the next middleware
+  console.error(error.stack);
+  next(error);
+}
+
+function errorHandler(error, req, res, next) {
+  // send error message to client
+  // message for gracefull error handling on app
+  res.status(500).send({ error: error.message });
+}
 
 // frontend routes =========================================================
 module.exports = function (app) {
-  // Example:
-  // app.get('/', function(req, res) {
-  //   console.log("root")
-  //   res.render('root'); // load view/root.html file
-  // });
+  // Github WebHook
+  app.post('/github', github.webHookReceiver);
+
+  // Error Logger/Handler
+  app.use(errorLogger);
+  app.use(errorHandler);
 };
