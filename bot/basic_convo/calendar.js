9b5462894644fbb2f235df0c18436626f2eceed4
Kris Stange <kris.stange2015@gmail.com>
2016-05-04 15:44:50 -0700

    Add google calendar integration
    Rename working files in more descriptive manner
    .gitignore
    bot/basic_convo/calendar.js
    bot/karabot.js
    calendar/googCalendar.js
    config/config-folder-placeholder.js
    package.json

---
+++ bot/basic_convo/calendar.js
@@ -0,0 +1,17 @@
+var googCal = require('../../calendar/googCalendar.js');
+
+function clist(bot, message) {
+  bot.reply(message, '_I\'m searching..._');
+  googCal.authCallFunction( function(data) {
+    bot.reply(message, 'Ok, here\'s the results:\n' + data)
+  }, 'calendar list');  
+}
+
+function ctoday(bot, message) {
+  googCal.authCallFunction( 'null', 'days events');
+}
+
+module.exports = {
+  clist: clist,
+  ctoday: ctoday
+};
