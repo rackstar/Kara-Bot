e8c630c063d7ee0f1492449abf3defe2db774069
Rocky Murdoch <rockysta009@gmail.com>
2016-05-06 13:02:33 -0700

    (test) Add repos test suite

--- spec/githubSpec.js
+++ spec/githubSpec.js
@@ -2,66 +2,62 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 var GitHubApi = require('github');
 var expect = chai.expect;
-var command;
+var messages;
+var botDirect;
+var botId;
 var output;
-var repo = process.env.testrepo;
+var repo;
 
 require('dotenv')
   .config();
 
-channel = process.env.channelId;
+repo = process.env.testrepo;
 botDirect = process.env.botDirect;
+botId = process.env.botId;
 
 chai.use(chaiHttp);
 
-function responseTest(input, output, done) {
-  var text;
+function sendCommand(input) {
   chai.request('https://slack.com/api/chat.postMessage')
-    .post('?token=' + process.env.me + '&channel=%40karabot&text=' + input + '&as_user=true&pretty=1')
-    .end(function testResponse(err, res) {
-      setTimeout(function waitRequest() {
-        chai.request('https://slack.com/api/im.history')
-          .get('?token=' + process.env.me + '&channel=' + process.env.botDirect + '&count=5&pretty=1')
-          .end(function assignText(err, res) {
-            if (err) {
-              console.log('err ', err);
-            } else {
-              text = res.body.messages[0].text;
-            }
-          });
-      }, 1200);
+    .post('?token=' + process.env.me + '&channel=' + botDirect + '&text=' + input + '&as_user=true&pretty=1')
+    .end(function sendCommandCb(err, res) {
+      if (err) console.log(err);
     });
-  setTimeout(function waitCheck() {
-    expect(text)
-      .to.equal(output);
-    done();
-  }, 2400);
 }
 
-function channelHistory(output, done) {
-  var text;
-  setTimeout(function waitRequestCH() {
-    chai.request('https://slack.com/api/im.history')
-      .get('?token=' + process.env.me + '&channel=' + process.env.botDirect + '&count=5&pretty=1')
-      .end(function assignText(err, res) {
-        if (err) {
-          console.log('err ', err);
-        } else {
-          // change text to check more than the first message
-          text = res.body.messages[0].text;
-        }
-      });
-  }, 1200);
-  setTimeout(function waitCheckCH() {
-    expect(text)
-      .to.equal(output);
-    done();
-  }, 2400);
+function channelHistory(count, cb) {
+  var messages;
+  // setTimeout(function CHtimeout() {
+  chai.request('https://slack.com/api/im.history')
+    .get('?token=' + process.env.me + '&channel=' + botDirect + '&count=' + count + '&pretty=1')
+    .end(function assignText(err, res) {
+      if (err) {
+        console.log(err);
+      } else {
+        messages = res.body.messages;
+        // filter response messages of the bot
+        messages = messages.filter(function(message) {
+          return message.user === botId;
+        });
+      }
+    });
+  return messages;
 }
+
+function findResponse(messages, expectation) {
+  var botResponse = messages.find(function(message) {
+    return message.text === expectation;
+  });
+  expect(botResponse.text)
+    .to.equal(expectation);
+}
+
 // If a test below fails, increase timeout thresholds to see if it is due to delay.
 // Pre-requisites (.env):
-// channelId=channelyouwishtotest (https://api.slack.com/methods/channels.list)
 // me=authorizationtoken (https://api.slack.com/docs/oauth-test-tokens)
+// botId=idofyourbot (https://api.slack.com/methods/users.list/test)
+// channelId=botsdirectmessagechannel (https://api.slack.com/methods/im.list/test)
+// use bot's Id to find its direct message channel with you
 // username=githubusername
 // password=githubpassword
 // testRepo=<githubusername>/<repotobetested>
@@ -87,42 +83,107 @@ beforeEach(function githubSetup() {
 //Repositories
 // lists repo
 describe('Lists Repos', function() {
-  describe('"show repo [number]"', function() {
-    it('should list all repositories for "show repos"', function(done) {
-      command = 'show repos';
-      output = '';
-      responseTest(command, output, done);
+  xdescribe('"show <number> repo"', function() {
+    var botMsg;
+    var count = 9;
+    // sendCommand('show repos');
+    // sendCommand('show 2 repos');
+    // sendCommand('show 1 repos');
+
+    setTimeout(function timeout1() {
+      chai.request('https://slack.com/api/im.history')
+        .get('?token=' + process.env.me + '&channel=' + botDirect + '&count=' + count + '&pretty=1')
+        .end(function assignText(err, res) {
+          if (err) {
+            console.log(err);
+          } else {
+            messages = res.body.messages;
+            // filter response messages of the bot
+            botMsg = messages.filter(function(message) {
+              return message.user === botId;
+            });
+          }
+        });
+    }, 3000);
+
+    xit('should list all repositories for "show repos"', function(done) {
+      this.timeout(4000);
+      setTimeout(function timeout2() {
+        output = 'Here are all your current repositories:';
+        findResponse(botMsg, output);
+      }, 4000);
+      setTimeout(done, 2000);
     });
-    it('should list all repositories for "repos"', function(done) {
-      command = 'repos';
-      output = '';
-      responseTest(command, output, done);
+    xit('should list 2 of the most recent repos on "show 2 repos"', function(done) {
+      this.timeout(4000);
+      setTimeout(function timeout2() {
+        output = 'Here are your 2 most recent repositories:';
+        findResponse(botMsg, output);
+      }, 4000);
+      setTimeout(done, 2000);
     });
-    it('should list 2 of the most recent repos on "show 2 repos"', function(done) {
-      command = 'show 2 repos';
-      output = '';
-      responseTest(command, output, done);
-    });
-    it('should list the most recent repository on "show 1 repos" or "show 1 repo"', function(done) {
-      command = 'show 1 repos';
-      output = '';
-      responseTest(command, output, done);
-      command = 'show 1 repo';
-      responseTest(command, output, done);
-    });
-    it('should round up arguments that has a decimal place greater than or equal to .5', function(done) {
-      command = 'show 1.793 repos';
-      output = '';
-      responseTest(command, output, done);
-    });
-    it('should round down arguments that has a decimal place less than .5', function(done) {
-      command = 'show 1.337 repos';
-      output = '';
-      responseTest(command, output, done);
+    xit('should list the most recent repository on "show 1 repos"', function(done) {
+      this.timeout(4000);
+      setTimeout(function timeout2() {
+        output = 'Here is your most recent repository:';
+        findResponse(botMsg, output);
+      }, 4000);
+      setTimeout(done, 2000);
     });
   });
 
-  describe('"help repos"', function() {
+  describe('"other passable repo instructions"', function() {
+    var botMsg;
+    var count = 9;
+
+    sendCommand('repos');
+    sendCommand('show 1.793 repos');
+    sendCommand('show 1.337 repos');
+
+    setTimeout(function timeout1() {
+      chai.request('https://slack.com/api/im.history')
+        .get('?token=' + process.env.me + '&channel=' + botDirect + '&count=' + count + '&pretty=1')
+        .end(function assignText(err, res) {
+          if (err) {
+            console.log(err);
+          } else {
+            messages = res.body.messages;
+            // filter response messages of the bot
+            botMsg = messages.filter(function(message) {
+              return message.user === botId;
+            });
+          }
+        });
+    }, 3000);
+
+    it('should list all repositories for "repos"', function(done) {
+      this.timeout(4000);
+      setTimeout(function timeout2() {
+        output = 'Here are all your current repositories:';
+        findResponse(botMsg, output);
+      }, 4000);
+      setTimeout(done, 2000);
+    });
+    it('should round up numbers that has a decimal place greater than or equal to .5', function(done) {
+      this.timeout(4000);
+      setTimeout(function timeout2() {
+        output = 'Here are your 2 most recent repositories:';
+        findResponse(botMsg, output);
+      }, 4000);
+      setTimeout(done, 2000);
+    });
+    it('should round down numbers that has a decimal place less than .5', function(done) {
+      this.timeout(4000);
+      setTimeout(function timeout2() {
+        output = 'Here is your most recent repository:';
+        findResponse(botMsg, output);
+      }, 4000);
+      setTimeout(done, 2000);
+    });
+  });
+
+
+  xdescribe('"help repos"', function() {
     it('should show repos instructions on "help repos"', function(done) {
       command = 'help repos';
       output = 'How to use show repo';
@@ -152,7 +213,7 @@ describe('Lists Repos', function() {
     });
   });
 
-  describe.skip('invalid commands', function() {
+  xdescribe('invalid commands', function() {
     it('should respond with an invalid command message when the argument is not a number', function(done) {
       command = 'show apo repos';
       output = 'invalid command, please check /repo help';
@@ -167,7 +228,7 @@ describe('Lists Repos', function() {
 });
 
 // watch repo
-describe('watch repo', function() {
+describe.skip('watch repo', function() {
   beforeEach(function() {
     // delete any webhook that is already there
   });
@@ -235,7 +296,7 @@ describe('watch repo', function() {
 });
 
 // unwatches repo
-describe('unwatch', function() {
+xdescribe('unwatch', function() {
   beforeEach(function() {
     // verify webhook is working
     // if working continue
