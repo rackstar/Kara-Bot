68a2157e6efaac1b98865c54514248b9d7534356
Aaron Hildebrand <aaron.hildebrand@gmail.com>
2016-05-07 14:07:22 -0700

    (feat) Add index.js, index.html and dependencies for React front-end

---
+++ public/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router'; // I believe we used hashHistory in Tyler's class
+import reducers from './reducers';
+import routes from './routes';
+import promise from 'redux-promise';
+
+const createStoreWithMiddleware = applyMiddleware(
+  promise
+)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>
+  , document.querySelector('.container'));
