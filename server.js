94e226b52f0bfd8fc237e7441d87283b376d60dd
Aaron Hildebrand <aaron.hildebrand@gmail.com>
2016-05-09 19:08:54 -0700

    (feat) Add server side updates for Auth0

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var bodyParser = require('body-parser');
 var http = require('http').Server(app);
 var dotenv = require('dotenv');
 var JiraClient = require('jira-connector');
-var jiraController = require('./server/jira/jiraController')
+var jiraController = require('./server/jira/jiraController');
+var jwt = require('express-jwt');
+
 
 // configuration ===========================================
 
@@ -13,12 +15,21 @@ var jiraController = require('./server/jira/jiraController')
 // heroku environment in production, etc...
 dotenv.load();
 
+// Auth0 requirement for API calls
+var jwtCheck = jwt({
+  secret: new Buffer(process.env.AUTH0_CLIENT_SECRET, 'base64'),
+  audience: process.env.AUTH0_CLIENT_ID
+});
+
 // public folder for images, css,... Where our react front-end will live
 app.use(express.static(__dirname + '/public'));
 
 // parsing
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing url encoded
+
+// Block users who aren't logged in from doing requests on api data
+app.use('/api/', jwtCheck);
 
 // routes
 require('./bot/config/routes')(app);
