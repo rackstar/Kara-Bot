ec6b05456cec1d179af846c543c832ff99cd49de
Aaron Hildebrand <aaron.hildebrand@gmail.com>
2016-05-14 12:11:37 -0700

    (feat) Update users_list.js to display list of users on team on sortable table

--- public/components/users_list.js
+++ public/components/users_list.js
@@ -1,31 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { fetchUsers, fetchUser } from '../actions/index';
+import { Link } from 'react-router';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
-export default class UsersList extends Component {
+class UsersList extends Component {
+  componentWillMount() {
+    if(this.props.userid) {
+      this.props.fetchUser(this.props.userid);
+    } else {
+      this.props.fetchUser();
+    }
+    this.props.fetchUsers();
+    this.setState({user:null});
+  }
+
+  getUser(user) {
+    this.props.fetchChat(user.slack_user_id);
+    this.setState({user:user});
+  }
+
+  isBot(cell, row) {
+    if(cell===true) {
+      return 'True';
+    } else {
+      return 'False';
+    }
+  }
 
   render() {
     return (
       <div>
         <h1>Users</h1>
-        <table className="table table-hover">
-          <thead>
-            <tr>
-              <th>User</th>
-              <th>Slack ID</th>
-              <th>GitHub</th>
-              <th>JIRA ID</th>
-              <th>Other?</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr><td>Temp</td><td>Temp</td><td>Temp</td><td>Temp</td><td>Temp</td></tr>
-            <tr><td>Temp</td><td>Temp</td><td>Temp</td><td>Temp</td><td>Temp</td></tr>
-            <tr><td>Temp</td><td>Temp</td><td>Temp</td><td>Temp</td><td>Temp</td></tr>
-            <tr><td>Temp</td><td>Temp</td><td>Temp</td><td>Temp</td><td>Temp</td></tr>
-            <tr><td>Temp</td><td>Temp</td><td>Temp</td><td>Temp</td><td>Temp</td></tr>
-            <tr><td>Temp</td><td>Temp</td><td>Temp</td><td>Temp</td><td>Temp</td></tr>
-          </tbody>
-        </table>
+        <BootstrapTable data={this.props.all} striped={true} hover={true} condensed={true} pagination={true} search={true}>
+          <TableHeaderColumn dataField="username" isKey={true} dataSort={true}>Username</TableHeaderColumn>
+          <TableHeaderColumn dataField="firstname" dataSort={true}>First Name</TableHeaderColumn>
+          <TableHeaderColumn dataField="lastname" dataSort={true}>Last Name</TableHeaderColumn>
+          <TableHeaderColumn dataField="email" dataSort={true}>Email</TableHeaderColumn>
+          <TableHeaderColumn dataField="slack_user_id" dataSort={true}>Slack User ID</TableHeaderColumn>
+          <TableHeaderColumn dataField="is_bot" dataSort={true} dataFormat={this.isBot}>Bot User?</TableHeaderColumn>
+        </BootstrapTable>
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return { all: state.users.all, user: state.users.user };
+}
+
+export default connect(mapStateToProps, { fetchUsers, fetchUser })(UsersList);
