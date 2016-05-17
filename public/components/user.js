aca0acbf7cc035a692aa4d1fd6c435474eadc25b
Aaron Hildebrand <aaron.hildebrand@gmail.com>
2016-05-16 19:04:24 -0700

    (feat) Update user.js to include channel data, column width, user data and export to CSV capabilities

--- public/components/user.js
+++ public/components/user.js
@@ -7,24 +7,57 @@ import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 class User extends Component {
   componentWillMount() {
     this.props.fetchUser();
+    this.setState({ready:false});
   }
   componentDidMount() {
     var id = this.props.params.id;
-    this.props.fetchUser(id);
+    var currContext=this;
+    this.props.fetchUser(id)
+      .then(()=> {
+        currContext.buildData(currContext.makeReady.bind(currContext));
+      });
+  }
+  allChannels={};
+  userInfo={};
+
+  buildData(callback) {
+    this.userInfo = this.props.user[this.props.user.length-2][0];
+    var channels = this.props.user[this.props.user.length-1];
+
+    for(var i = 0; i < channels.length; i++) {
+      this.allChannels[channels[i].slack_channel_id] = channels[i].channel_name;
+    }
+    callback();
+  }
+  makeReady() {
+    console.log(this.userInfo);
+    this.setState({ready:true});
+  }
+
+  channelName(cell, row) {
+    return this.allChannels[cell];
   }
 
   render() {
-    return (
-      <div>
-        <h1>User: {this.props.user[0].username}</h1>
-        <BootstrapTable data={this.props.user} striped={true} hover={true} condensed={true} pagination={true} search={true}>
-          <TableHeaderColumn dataField="message_id" isKey={true} dataSort={true}>Message ID</TableHeaderColumn>
-          <TableHeaderColumn dataField="channel_id" dataSort={true}>Channel ID</TableHeaderColumn>
-          <TableHeaderColumn dataField="slack_ts" dataSort={true}>Timestamp</TableHeaderColumn>
-          <TableHeaderColumn dataField="message_text" dataSort={true}>Message</TableHeaderColumn>
-        </BootstrapTable>
-      </div>
-    );
+    if(this.state.ready) {
+      return (
+        <div>
+          <h1>User: {this.userInfo.username}</h1>
+          <h5>First Name: {this.userInfo.firstname}</h5>
+          <h5>Last Name: {this.userInfo.lastname}</h5>
+          <h5>Email: {this.userInfo.email}</h5>
+          <h5>Bot User: {this.userInfo.is_bot.toString().toUpperCase()}</h5>
+          <BootstrapTable data={this.props.user} striped={true} hover={true} condensed={true} pagination={true} search={true} exportCSV={true}>
+            <TableHeaderColumn dataField="message_id" width="50" isKey={true} dataSort={true}>ID</TableHeaderColumn>
+            <TableHeaderColumn dataField="channel_id" width="100" dataSort={true} dataFormat={this.channelName.bind(this)}>Channel</TableHeaderColumn>
+            <TableHeaderColumn dataField="slack_ts" width="100" dataSort={true}>Timestamp</TableHeaderColumn>
+            <TableHeaderColumn dataField="message_text" dataSort={true}>Message</TableHeaderColumn>
+          </BootstrapTable>
+        </div>
+      );
+    } else {
+      return(<h1>Loading...</h1>);
+    }
   }
 }
 
