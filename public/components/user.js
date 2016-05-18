0c1d2cf5b0c543daa47228f97d8fd759c14a6a68
Aaron Hildebrand <aaron.hildebrand@gmail.com>
2016-05-18 15:23:10 -0700

    (style) Update color styling for charts and add Jumbotron to user.js

--- public/components/user.js
+++ public/components/user.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { fetchUser } from '../actions/index';
 import { Link } from 'react-router';
+import { Jumbotron } from 'react-bootstrap';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 class User extends Component {
@@ -42,11 +43,13 @@ class User extends Component {
     if(this.state.ready) {
       return (
         <div>
-          <h1>User: {this.userInfo.username}</h1>
-          <h5>First Name: {this.userInfo.firstname}</h5>
-          <h5>Last Name: {this.userInfo.lastname}</h5>
-          <h5>Email: {this.userInfo.email}</h5>
-          <h5>Bot User: {this.userInfo.is_bot.toString().toUpperCase()}</h5>
+          <Jumbotron>
+            <h1>User: {this.userInfo.username}</h1>
+            <h5>First Name: {this.userInfo.firstname}</h5>
+            <h5>Last Name: {this.userInfo.lastname}</h5>
+            <h5>Email: {this.userInfo.email}</h5>
+            <h5>Bot User: {this.userInfo.is_bot.toString().toUpperCase()}</h5>
+          </Jumbotron>
           <BootstrapTable data={this.props.user} striped={true} hover={true} condensed={true} pagination={true} search={true} exportCSV={true}>
             <TableHeaderColumn dataField="message_id" width="50" isKey={true} dataSort={true}>ID</TableHeaderColumn>
             <TableHeaderColumn dataField="channel_id" width="100" dataSort={true} dataFormat={this.channelName.bind(this)}>Channel</TableHeaderColumn>
