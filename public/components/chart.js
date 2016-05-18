639e016a0c3ad7b741a515c76a816832f7d7e891
Kris Stange <kris.stange2015@gmail.com>
2016-05-18 00:44:07 -0700

    (feat) Pull data from watson and load into chart
    deleted:    chart/bubble.css
    deleted:    chart/bubble.html
    deleted:    chart/bubble.js
    deleted:    chart/bubble2.js
    deleted:    chart/buildDataInput.js
    deleted:    chart/index.html
    deleted:    chart/js/app.js
    deleted:    chart/js/app.min.js
    modified:   public/components/chart.js

--- public/components/chart.js
+++ public/components/chart.js
@@ -7,45 +7,42 @@ import D3 from 'D3';
 export default class Chart extends Component {
     
   componentWillMount() {
-    
     this.setState({ready:false});
-    
-    console.log('####### Entering ########')
-    var currentThis = this;
-    this.d3Data = this.getWatsonData(currentThis.resultsBack.bind(currentThis));
-    console.log(this.d3Data)
-    console.log('######## exiting ##########')
-    // this.resultsBack();
+    this.getWatsonData();
   }
   
-  resultsBack() {
+  resultsBack(data) {
+    this.d3Data = data;
     this.setState({ready:true});
   }
   
-  getWatsonData(callback) {
-
-   var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "/api/watson/channel",
-    "method": "POST",
-    "headers": {
-      "cache-control": "no-cache",
-      "postman-token": "c5a32e6d-7f64-5eaa-d435-50d5af557090",
-      "content-type": "application/x-www-form-urlencoded"
-    },
-    "data": {
-      "channel": "C155RNX46"
-    }
-  }
+  getWatsonData() {
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "/api/watson/channel",
+      "method": "POST",
+      "headers":
+        {
+          "cache-control": "no-cache",
+          "content-type": "application/x-www-form-urlencoded"
+        },
+      "data":
+        {
+          "channel": "C14CAT2HE"
+        }
+    };
   
   // var x = $.ajax(settings).done(function (response,callback) {
-  var x = $.ajax(settings).done(function () {
-    callback();
+  var callback = this.resultsBack.bind(this);
+  $.ajax(settings).done(function (response) {
+    // this.d3Data = processData(response);
+    // console.log(this.d3Data);
+    callback(processData(response));
   });
-      var json = {"Social Tone":{"Openness":.079,"Conscientiousness":.560,"Extraversion":.951,"Agreeableness":.763,"Emotional Range":.375}}
-      var data = processData(json);
-      return data;
+      // var json = {"Social Tone":{"Openness":.079,"Conscientiousness":.560,"Extraversion":.951,"Agreeableness":.763,"Emotional Range":.375}}
+      // var data = processData(json);
+      // this.d3Data = data;
   }
 
   render() {
@@ -71,6 +68,7 @@ export default class Chart extends Component {
     
     //bubbles needs very specific format, convert data to this.
     // var data = processData(json);
+    console.log(this.d3Data);
 
     var nodes = bubble.nodes({children:this.d3Data}).filter(function(d) { return !d.children; });
 
@@ -116,15 +114,49 @@ export default class Chart extends Component {
   }
 }
   
+// function processData(data) {
+//   var obj = data[Object.keys(data)[0]];
+
+//   var newDataSet = [];
+
+//   for(var key in obj) {
+//     newDataSet.push({name: key, value: obj[key]});
+//   }
+//   return newDataSet;
+// }
 function processData(data) {
-  var obj = data[Object.keys(data)[0]];
-
-  var newDataSet = [];
-
-  for(var key in obj) {
-    newDataSet.push({name: key, value: obj[key]});
+  var wrapper = [];
+  for (var i = 0; i < data.length; i++) {
+    // if text is undefined then record has data
+    if (!data[i].text) {
+      wrapper.push(data[i].tone[2])
+    }
   }
-  return newDataSet;
+  
+  var results = [];
+  for (var i = 0; i < wrapper.length; i++) {
+    results.push(wrapper[i].tones);
+  }
+  
+  for (var i = 0; i < results[0].length; i++) {
+    var accum = 0
+    for (var j = 1; j < results.length; j++) {
+      accum += results[j][i].score;
+    }
+    results[0][i].score = (results[0][i].score + accum) / results.length;
+  }
+  
+  wrapper = [];
+  results = results[0];
+  for (var i = 0; i < results.length; i++) {
+    var obj = {};
+    obj.name = results[i].tone_name;
+    obj.id = results[i].tone_id;
+    obj.value = results[i].score;
+    wrapper.push(obj);
+  }
+  
+  return wrapper;
 }
 
 // function getWatsonData(callback) {
