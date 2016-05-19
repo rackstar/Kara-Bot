be16acebe8f76b128001831525680a6926a0bc10
Aaron Hildebrand <aaron.hildebrand@gmail.com>
2016-05-18 19:00:45 -0700

    (fix) Update chart.js to fix capitalization error on d3 import

--- public/components/chart.js
+++ public/components/chart.js
@@ -2,7 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import Faux from 'react-faux-dom';
-import D3 from 'D3';
+import d3 from 'd3';
 
 export default class Chart extends Component {
 
